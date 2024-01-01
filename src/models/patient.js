704ef65e929eb60276920695c022d1375580b636
Alpha <syedaarishshah@gmail.com>
2024-01-01 23:31:33 +0530

    updated patient model

--- src/models/patient.js
+++ src/models/patient.js
@@ -1,52 +1,81 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const userSchema = new mongoose.Schema(
+    {
+        name:
+        {
+            type: String,
+            required: true,
+            trim: true,
+        },
 
 
         email:
         {
             type: String,
-            required: false,
-            validator(value)
+            unique: true, // this will make sure that every email different
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value)
             {
                 if (!validator.isEmail(value))
                 {
-                    throw new Error('Email is invalid');
+                    throw new Error('Email is invalid')
                 }
-            },
-            trim: true,
+            }
         },
-
 
         password:
         {
             type: String,
-            minlength: 6,
+            minlength: 8,
             required: true,
             trim: true,
         },
 
-        age:
+        DOB:
         {
-            type: Number,
+            type: Date,
             required: true,
-            trim: true,
+            validate(value)
+            {
+                const currentDate = new Date();
+
+                value.setHours(0, 0, 0, 0);
+                currentDate.setHours(0, 0, 0, 0);
+
+                if (value > currentDate)
+                {
+                    throw new Error('Invalid age - Date of birth cannot be in the future');
+                }
+            }
         },
 
         gender:
         {
             type: String,
             required: true,
+            enum: ['Male', 'Female', 'Other', 'Prefer not to say'],
             trim: true,
-        }
+            default: 'Prefer not to say'
+        },
+    },
 
-    })
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+userSchema.virtual('age').get(function ()
+{
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - this.DOB.getFullYear();
+    return age;
+});
 
 const patient = mongoose.model('patient', userSchema)
 
