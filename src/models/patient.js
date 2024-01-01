967208da327de3cf092f6e5f9ff13f3cd1e8d74e
humma irshad <hummairshad48@gmail.com>
2024-01-01 23:22:25 +0530

    Make changes to patient route and add 'read patient' route

--- src/models/patient.js
+++ src/models/patient.js
@@ -1,57 +1,44 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema(
-    {
-        name:
-        {
-            type: String,
-            required: true,
-            trim: true,
-        },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
+  email: {
+    type: String,
+    required: false,
+    validator(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+    trim: true,
+  },
 
-        email:
-        {
-            type: String,
-            required: false,
-            validator(value)
-            {
-                if (!validator.isEmail(value))
-                {
-                    throw new Error('Email is invalid');
-                }
-            },
-            trim: true,
-        },
+  password: {
+    type: String,
+    minlength: 6,
+    required: true,
+    trim: true,
+  },
 
+  age: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
 
-        password:
-        {
-            type: String,
-            minlength: 6,
-            required: true,
-            trim: true,
-        },
+  gender: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
-        age:
-        {
-            type: Number,
-            required: true,
-            trim: true,
-        },
-
-        gender:
-        {
-            type: String,
-            required: true,
-            trim: true,
-        }
-
-    })
-
-const patient = mongoose.model('patient', userSchema)
+const patient = mongoose.model("patient", userSchema);
 
 module.exports = patient;
-
-
--- src/routers/patient.js
+++ src/routers/patient.js
@@ -1,34 +1,51 @@
-const express = require('express');
-const Patient = require('../models/patient');
-const auth = require('../middleware/auth');
-const router = new express.Router();
+const express = require("express");
+
+const Patient = require("../models/patient");
+
+const patientRouter = new express.Router();
 
 // createAccount() @humma-irshad
 
 // Create a new patient
-router.post('/patients', async (req, res) => 
-{
-    try
-    {
-        const { name, email, password, age, gender } = req.body;
+patientRouter.post("/patient", async (req, res) => {
+  try {
+    const { name, email, password, age, gender } = req.body;
 
-        if (!name || !email || !password || !age || !gender)
-        {
-            return res.status(400).send({ error: 'Name, email, password, age and gender are required' });
-        }
-
-        // Implement logic to create a new patient in the database
-        const newpatient = await Patient.create({ name, email, password, age, gender });
-
-        res.status(201).send({ newpatient });
-    } catch (e)
-    {
-        console.error('Create error:', e);
-        res.status(500).send({ error: 'Create failed' });
+    if (!name || !email || !password || !age || !gender) {
+      return res
+        .status(400)
+        .send({ error: "Name, email, password, age and gender are required" });
     }
+
+    // Implement logic to create a new patient in the database
+    const newPatient = await Patient.create({
+      name,
+      email,
+      password,
+      age,
+      gender,
+    });
+
+    res.status(201).send({ newPatient });
+  } catch (e) {
+    console.error("Create error:", e);
+    res.status(500).send({ statusCode: 500, messgae: "Create failed" });
+  }
 });
 
 // readAccount() @humma-irshad - @KhushbooHamid
+patientRouter.get("/patient", async (_, res) => {
+  const patients = await Patient.find();
+
+  if (!patients)
+    res.send({
+      statusCode: 404,
+      status: "Not Found",
+      messgae: "No records found",
+    });
+
+  res.status(200).send({ statusCode: 200, status: "OK", data: patients });
+});
 
 // updateAccount()
 
@@ -38,27 +55,24 @@ router.post('/patients', async (req, res) =>
 // deleteAccount() @KhushbooHamid
 
 // Delete a patient by ID
-router.delete('/patients/:id', async (req, res) =>
-{
-    try
-    {
-        const patientId = req.params.id;
+patientRouter.delete("/patient/:id", async (req, res) => {
+  try {
+    const patientId = req.params.id;
 
-        const deletedpatient = await Patient.findByIdAndDelete(patientId);
+    const deletedpatient = await Patient.findByIdAndDelete(patientId);
 
-        if (!deletedpatient)
-        {
-            return res.status(404).send({ error: 'patient not found' });
-        }
-
-        res.send({ message: `patient with ID ${patientId} deleted successfully` });
-    } catch (e)
-    {
-        console.error('Delete error:', e);
-        res.status(500).send({ error: 'Delete failed' });
+    if (!deletedpatient) {
+      return res.status(404).send({ error: "patient not found" });
     }
-});
 
+    // when deleting a resource only a status code of 204 is sent
+    res.status(204);
+  } catch (e) {
+    console.error("Delete error:", e);
+    res.status(500).send({ error: "Delete failed" });
+  }
+});
 
 // chooseDoctor()
 // viewDiagnosisAndMed()
+module.exports = patientRouter;
