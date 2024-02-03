8637c281b43d35230a024834d7c67ebab8bcb73f
humma irshad <hummairshad48@gmail.com>
2024-02-03 12:55:17 +0530

    update. Update Patient schema and corresponding patient routes

--- src/models/patient.js
+++ src/models/patient.js
@@ -4,218 +4,212 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const addressSchema = new mongoose.Schema({
-    street:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    city:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    state:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    pinCode:
-    {
-        type: String,
-        required: true,
-        trim: true,
-    },
+  street: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  pinCode: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
 
 const patientSchema = new mongoose.Schema(
-    {
-        name:
-        {
-            type: String,
-            required: true,
-            trim: true,
-        },
-
-        address:
-        {
-            type: addressSchema,
-            required: true,
-        },
-
-        email:
-        {
-            type: String,
-            unique: true, // this will make sure that every email different
-            required: true,
-            trim: true,
-            lowercase: true,
-            validate(value)
-            {
-                if (!validator.isEmail(value))
-                {
-                    throw new Error("Email is invalid");
-                }
-            },
-        },
-
-        password:
-        {
-            type: String,
-            minlength: 8,
-            maxlength: 64,
-            required: true,
-            trim: true,
-        },
-
-        DOB:
-        {
-            type: Date,
-            required: true,
-            validate(value)
-            {
-                const currentDate = new Date();
-
-                value.setHours(0, 0, 0, 0);
-                currentDate.setHours(0, 0, 0, 0);
-
-                if (value > currentDate)
-                {
-                    throw new Error(
-                        "Invalid age - Date of birth cannot be in the future"
-                    );
-                }
-            },
-        },
-
-        gender:
-        {
-            type: String,
-            required: true,
-            enum: ["Male", "Female", "Other", "Prefer not to say"],
-            trim: true,
-            default: "Prefer not to say",
-        },
-
-        assignedDoctors: [
-            {
-                doctor:
-                {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "doctor",
-                },
-                name: String, 
-                email: String
-                
-            },
-        ],
-
-        tokens:
-            [
-                {
-                    token:
-                    {
-                        type: String,
-                        required: true,
-                    },
-                }
-            ],
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
 
-    {
-        timestamps: true,
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true },
-    }
+    address: {
+      type: addressSchema,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true, // this will make sure that every email different
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+
+    password: {
+      type: String,
+      minlength: 8,
+      maxlength: 64,
+      required: true,
+      trim: true,
+    },
+
+    DOB: {
+      type: Date,
+      required: true,
+      validate(value) {
+        const currentDate = new Date();
+
+        value.setHours(0, 0, 0, 0);
+        currentDate.setHours(0, 0, 0, 0);
+
+        if (value > currentDate) {
+          throw new Error(
+            "Invalid age - Date of birth cannot be in the future"
+          );
+        }
+      },
+    },
+
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female", "Other", "Prefer not to say"],
+      trim: true,
+      default: "Prefer not to say",
+    },
+
+    relationshipStatus: {
+      type: String,
+      required: true,
+      enum: ["Single", "Engaged", "Married"],
+      default: "Single",
+    },
+
+    religion: {
+      type: String,
+      required: true,
+      default: "None",
+    },
+
+    occupation: {
+      type: String,
+      required: true,
+      default: "Unemployed",
+    },
+
+    assignedDoctors: [
+      {
+        doctor: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "doctor",
+        },
+        name: String,
+        email: String,
+      },
+    ],
+
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 // Virtual property for patient medical record and prescription, to link patient with medical record and prescription
 // Add after defining medical record and prescription models
 
 // Age virtual property
-patientSchema.virtual("age").get(function ()
-{
-    const currentDate = new Date();
-    const birthDate = new Date(this.DOB);
+patientSchema.virtual("age").get(function () {
+  const currentDate = new Date();
+  const birthDate = new Date(this.DOB);
 
-    let ageYears = currentDate.getFullYear() - birthDate.getFullYear();
-    let ageMonths = currentDate.getMonth() - birthDate.getMonth();
-    let ageDays = currentDate.getDate() - birthDate.getDate();
+  let ageYears = currentDate.getFullYear() - birthDate.getFullYear();
+  let ageMonths = currentDate.getMonth() - birthDate.getMonth();
+  let ageDays = currentDate.getDate() - birthDate.getDate();
 
-    if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0))
-    {
-        ageYears--;
-        ageMonths = (ageMonths + 12) % 12;
-    }
+  if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+    ageYears--;
+    ageMonths = (ageMonths + 12) % 12;
+  }
 
-    if (ageDays < 0)
-    {
-        const lastDayOfPreviousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-        ageDays += lastDayOfPreviousMonth;
-        ageMonths = ageMonths - 1 < 0 ? 11 : ageMonths - 1;
-    }
+  if (ageDays < 0) {
+    const lastDayOfPreviousMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    ).getDate();
+    ageDays += lastDayOfPreviousMonth;
+    ageMonths = ageMonths - 1 < 0 ? 11 : ageMonths - 1;
+  }
 
-    return { years: ageYears, months: ageMonths, days: ageDays };
+  return { years: ageYears, months: ageMonths, days: ageDays };
 });
 
 // hide private data when sending patient object
-patientSchema.methods.toJSON = function ()
-{
-    const patient = this;
-    const patientObject = patient.toObject();
+patientSchema.methods.toJSON = function () {
+  const patient = this;
+  const patientObject = patient.toObject();
 
-    delete patientObject.password;
-    delete patientObject.tokens;
+  delete patientObject.password;
+  delete patientObject.tokens;
 
-    return patientObject;
+  return patientObject;
 };
 
-patientSchema.methods.generateAuthToken = async function (next)
-{
-    const patient = this;
-    const token = jwt.sign({ _id: patient.id.toString() }, 'thisismynewcourse')
+patientSchema.methods.generateAuthToken = async function (next) {
+  const patient = this;
+  const token = jwt.sign({ _id: patient.id.toString() }, "thisismynewcourse");
 
-    patient.tokens = patient.tokens.concat({ token })
+  patient.tokens = patient.tokens.concat({ token });
 
-    await patient.save()
+  await patient.save();
 
-    return token;
-}
+  return token;
+};
 
 // Authentication method for the patient model
-patientSchema.statics.findByCredentials = async function (email, password)
-{
-    const patient = await this.findOne({ email });
+patientSchema.statics.findByCredentials = async function (email, password) {
+  const patient = await this.findOne({ email });
 
-    if (!patient)
-    {
-        throw new Error("Unable to login");
-    }
+  if (!patient) {
+    throw new Error("Unable to login");
+  }
 
-    const isMatch = await bcrypt.compare(password, patient.password);
+  const isMatch = await bcrypt.compare(password, patient.password);
 
-    if (!isMatch)
-    {
-        throw new Error("Unable to login");
-    }
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
-    return patient;
+  return patient;
 };
 
-
 // Hash password before saving
-patientSchema.pre("save", async function (next)
-{
-    const patient = this;
+patientSchema.pre("save", async function (next) {
+  const patient = this;
 
-    if (patient.isModified("password"))
-    {
-        patient.password = await bcrypt.hash(patient.password, 8);
-    }
+  if (patient.isModified("password")) {
+    patient.password = await bcrypt.hash(patient.password, 8);
+  }
 
-    next();
+  next();
 });
 
 // Delete patient medical record and prescription when patient is removed
--- src/routers/patient.js
+++ src/routers/patient.js
@@ -21,139 +21,153 @@ const router = new express.Router();
 // viewDiagnosisAndMed() - @AarishShah
 
 // Sign Up Route
-router.post("/patient/signup", async (req, res) =>
-{
-  try
-  {
-    const { name, address, email, password, DOB, gender } = req.body;
+router.post("/patient/signup", async (req, res) => {
+  try {
+    const {
+      name,
+      address,
+      email,
+      password,
+      DOB,
+      gender,
+      relationshipStatus,
+      religion,
+      occupation,
+    } = req.body;
 
     const missingFields = [];
-    if (!name) missingFields.push('name');
-    if (!address) missingFields.push('address');
-    if (!email) missingFields.push('email');
-    if (!password) missingFields.push('password');
-    if (!DOB) missingFields.push('date of birth (DOB)');
-    if (!gender) missingFields.push('gender');
-
-    if (missingFields.length > 0)
-    {
-      return res.status(400).send({ error: `The following field(s) are required and missing: ${missingFields.join(', ')}. Please ensure all fields are filled out correctly.` });
+    if (!name) missingFields.push("name");
+    if (!address) missingFields.push("address");
+    if (!email) missingFields.push("email");
+    if (!password) missingFields.push("password");
+    if (!DOB) missingFields.push("date of birth (DOB)");
+    if (!gender) missingFields.push("gender");
+    if (!relationshipStatus) missingFields.push("relationshipStatus");
+    if (!religion) missingFields.push("religion");
+    if (!occupation) missingFields.push("occupation");
+    if (missingFields.length > 0) {
+      return res.status(400).send({
+        error: `The following field(s) are required and missing: ${missingFields.join(
+          ", "
+        )}. Please ensure all fields are filled out correctly.`,
+      });
     }
 
-    const newPatient = await Patient.create({ name, address, email, password, DOB, gender });
+    const newPatient = await Patient.create({
+      name,
+      address,
+      email,
+      password,
+      DOB,
+      gender,
+      relationshipStatus,
+      religion,
+      occupation,
+    });
     const token = await newPatient.generateAuthToken();
 
     res.status(201).send({ newPatient, token });
-  } catch (e)
-  {
+  } catch (e) {
     // console.error("Signup error:", e);
     res.status(500).send({ error: "Failed to create a new user." });
   }
 });
 
 // Login Route
-router.post("/patient/login", async (req, res) =>
-{
-  try
-  {
-    const patient = await Patient.findByCredentials(req.body.email, req.body.password);
+router.post("/patient/login", async (req, res) => {
+  try {
+    const patient = await Patient.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
     const token = await patient.generateAuthToken();
     res.status(202).send({ patient, token });
-  } catch (error)
-  {
+  } catch (error) {
     // console.error("Login error:", error);
     res.status(400).send({ error: "Login failed" });
   }
 });
 
 // Update Route
-router.patch("/patient/me", auth, async (req, res) =>
-{
+router.patch("/patient/me", auth, async (req, res) => {
   const updates = Object.keys(req.body); // returns the keys of the json object as an array
-  const allowedUpdates = ['name', 'address', 'email', 'password', 'gender'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+  const allowedUpdates = [
+    "name",
+    "address",
+    "email",
+    "password",
+    "gender",
+    "relationshipStatus",
+    "religion",
+    "occupation",
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-  if (!isValidOperation)
-  {
-    return res.status(404).send({ error: 'Invalid updates!' });
+  if (!isValidOperation) {
+    return res.status(404).send({ error: "Invalid updates!" });
   }
 
-  try
-  {
-    updates.forEach((update) => req.patient[update] = req.body[update]);
+  try {
+    updates.forEach((update) => (req.patient[update] = req.body[update]));
     await req.patient.save();
     res.status(200).send(req.patient);
-  }
-  catch (error)
-  {
+  } catch (error) {
     // console.error("Update error:", error);
     res.status(400).send({ error: "Update failed" });
   }
 });
 
 // Delete Route
-router.delete("/patient/me", auth, async (req, res) =>
-{
-  try
-  {
+router.delete("/patient/me", auth, async (req, res) => {
+  try {
     const patientId = req.patient._id;
     const deletedPatient = await Patient.findByIdAndDelete(patientId);
     // make sure to delete all prescriptions and medical records associated with the patient
-    res.send({ message: 'Account deleted successfully.' });
-  } catch (error)
-  {
+    res.send({ message: "Account deleted successfully." });
+  } catch (error) {
     // console.error("Delete error:", error);
     res.status(500).send({ error: "Delete failed" });
   }
 });
 
 // Logout Route
-router.post("/patient/logout", auth, async (req, res) =>
-{
-  try
-  {
-    req.patient.tokens = req.patient.tokens.filter((token) => { return token.token !== req.token; });
+router.post("/patient/logout", auth, async (req, res) => {
+  try {
+    req.patient.tokens = req.patient.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
     await req.patient.save();
     res.send({ message: "Logout successful" });
-  }
-
-  catch (e)
-  {
+  } catch (e) {
     // console.error("Logout error:", e);
     res.status(500).send({ error: "Logout failed" });
   }
 });
 
 // Logout All Route - Logout a patient from all devices
-router.post("/patient/logoutall", auth, async (req, res) =>
-{
-  try
-  {
+router.post("/patient/logoutall", auth, async (req, res) => {
+  try {
     req.patient.tokens = [];
     await req.patient.save();
     res.send({ message: "Logout successful from all instances." });
-  }
-  catch (error)
-  {
+  } catch (error) {
     // console.error("Logout error:", error);
     res.status(500).send({ error: "Logout failed" });
   }
 });
 
 // Read Patient Route
-router.get("/patient/me", auth, async (req, res) =>
-{
+router.get("/patient/me", auth, async (req, res) => {
   res.send(req.patient);
 });
 
 // assignDoctor Route - assign a doctor to the patient's assigned doctors list using the doctor's email
-router.post("/patient/assignDoctor", auth, async (req, res) =>
-{
-  try
-  {
+router.post("/patient/assignDoctor", auth, async (req, res) => {
+  try {
     const doctor = await Doctor.findOne({ email: req.body.email });
-    if (!doctor)
-    {
+    if (!doctor) {
       return res.status(404).send({ error: "Doctor not found" });
     }
 
@@ -163,35 +177,32 @@ router.post("/patient/assignDoctor", auth, async (req, res) =>
 
     // Check if the doctor is already assigned
     const isAlreadyAssigned = req.patient.assignedDoctors.some(
-      assignedDoc => assignedDoc.doctor.toString() === doctorId.toString()
+      (assignedDoc) => assignedDoc.doctor.toString() === doctorId.toString()
     );
 
-    if (isAlreadyAssigned)
-    {
+    if (isAlreadyAssigned) {
       return res.status(400).send({ error: "Doctor already assigned" });
     }
 
-    req.patient.assignedDoctors = req.patient.assignedDoctors.concat({ doctor: doctorId, name: doctorName, email: doctorEmail });
+    req.patient.assignedDoctors = req.patient.assignedDoctors.concat({
+      doctor: doctorId,
+      name: doctorName,
+      email: doctorEmail,
+    });
     await req.patient.save();
 
     res.status(201).send({ message: "Doctor assigned successfully" });
-
-  }
-  catch (error)
-  {
+  } catch (error) {
     // console.error("Doctor assignment error:", error);
     res.status(400).send({ error: "Doctor assignment failed" });
   }
 });
 
 // removeDoctor Route - remove a doctor from the patient's assigned doctors list using the doctor's email
-router.delete("/patient/removeDoctor", auth, async (req, res) =>
-{
-  try
-  {
+router.delete("/patient/removeDoctor", auth, async (req, res) => {
+  try {
     const doctor = await Doctor.findOne({ email: req.body.email });
-    if (!doctor)
-    {
+    if (!doctor) {
       return res.status(404).send({ error: "Doctor not found" });
     }
 
@@ -199,33 +210,28 @@ router.delete("/patient/removeDoctor", auth, async (req, res) =>
 
     // Check if the doctor is already assigned
     const isAssigned = req.patient.assignedDoctors.some(
-      assignedDoc => assignedDoc.doctor.toString() === doctorId.toString()
+      (assignedDoc) => assignedDoc.doctor.toString() === doctorId.toString()
     );
 
-    if (!isAssigned)
-    {
+    if (!isAssigned) {
       return res.status(400).send({ error: "Doctor not assigned" });
     }
 
     req.patient.assignedDoctors = req.patient.assignedDoctors.filter(
-      assignedDoc => assignedDoc.doctor.toString() !== doctorId.toString()
+      (assignedDoc) => assignedDoc.doctor.toString() !== doctorId.toString()
     );
     await req.patient.save();
 
     res.status(200).send({ message: "Doctor removed successfully" });
-
-  }
-  catch (error)
-  {
+  } catch (error) {
     // console.error("Doctor removal error:", error);
     res.status(400).send({ error: "Doctor removal failed" });
   }
 });
 
-
 // post?
 
 // viewDiagnosisAndMed()
-router.get("/patient/:id", async (req, res) => { });
+router.get("/patient/:id", async (req, res) => {});
 
 module.exports = router;
