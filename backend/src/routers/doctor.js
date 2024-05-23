6741046e270a7cb0d2444d4b48a1870b3bb86f07
Alpha <syedaarishshah@gmail.com>
2024-05-23 18:18:37 +0530

    set formatting to compact look

--- backend/src/routers/doctor.js
+++ backend/src/routers/doctor.js
@@ -4,7 +4,8 @@ const Doctor = require("../models/doctor");
 const auth = require("../middleware/auth");
 const checkExpiration = require("../middleware/check-expiration");
 const router = express.Router();
-const { assignDoctor, removeDoctor } = require("../utils/assignment");
+const { assignDoctor, removeDoctor } = require('../utils/assignment');
+
 
 // completed:
 
@@ -25,182 +26,186 @@ const { assignDoctor, removeDoctor } = require("../utils/assignment");
 // when a doctor is assigned himself/herself to a patient, the patient should get a notification which when accepted, the doctor is added to the patient's assignedDoctors array
 
 // Sign Up Route
-router.post("/doctor/signup", async (req, res) => {
-  try {
-    const {
-      name,
-      email,
-      password,
-      gender,
-      specialization,
-      yearsOfExperience,
-      qualifications,
-    } = req.body;
+router.post("/doctor/signup", async (req, res) =>
+{
+  try
+  {
+    const { name, email, password, gender, specialization, yearsOfExperience, qualifications } = req.body;
 
     const missingFields = [];
-    if (!name) missingFields.push("name");
-    if (!email) missingFields.push("email");
-    if (!password) missingFields.push("password");
-    if (!gender) missingFields.push("gender");
-    if (!specialization) missingFields.push("specialization");
-    if (!yearsOfExperience) missingFields.push("years of experience");
-    if (!qualifications) missingFields.push("qualifications");
+    if (!name) missingFields.push('name');
+    if (!email) missingFields.push('email');
+    if (!password) missingFields.push('password');
+    if (!gender) missingFields.push('gender');
+    if (!specialization) missingFields.push('specialization');
+    if (!yearsOfExperience) missingFields.push('years of experience');
+    if (!qualifications) missingFields.push('qualifications');
 
-    if (missingFields.length > 0) {
-      return res
-        .status(400)
-        .send({
-          error: `The following field(s) are required and missing: ${missingFields.join(
-            ", "
-          )}. Please ensure all fields are filled out correctly.`,
-        });
+    if (missingFields.length > 0)
+    {
+      return res.status(400).send({ error: `The following field(s) are required and missing: ${missingFields.join(', ')}. Please ensure all fields are filled out correctly.` });
     }
 
-    const newDoctor = await Doctor.create({
-      name,
-      email,
-      password,
-      gender,
-      specialization,
-      yearsOfExperience,
-      qualifications,
-    });
+    const newDoctor = await Doctor.create({ name, email, password, gender, specialization, yearsOfExperience, qualifications });
     const token = await newDoctor.generateAuthToken();
 
     res.status(201).send({ newDoctor, token });
-  } catch (e) {
+  } catch (e)
+  {
+    // console.error("Create error:", e);
     res.status(500).send({ error: "Create failed" });
   }
 });
 
 // Login Route
-router.post("/doctor/login", async (req, res) => {
-  try {
-    const doctor = await Doctor.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
+router.post("/doctor/login", async (req, res) =>
+{
+  try
+  {
+    const doctor = await Doctor.findByCredentials(req.body.email, req.body.password);
     const token = await doctor.generateAuthToken();
     res.status(202).send({ doctor, token });
-  } catch (error) {
+  } catch (error)
+  {
+    // console.error("Login error:", error);
     res.status(400).send({ error: "Login failed" });
   }
 });
 
 // Update Route
-router.patch("/doctor/me", checkExpiration, auth, async (req, res) => {
+router.patch("/doctor/me", checkExpiration, auth, async (req, res) =>
+{
   const updates = Object.keys(req.body);
-  const allowedUpdates = [
-    "name",
-    "email",
-    "password",
-    "gender",
-    "specialization",
-    "yearsOfExperience",
-    "qualifications",
-  ];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
+  const allowedUpdates = ['name', 'email', 'password', 'gender', 'specialization', 'yearsOfExperience', 'qualifications'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-  if (!isValidOperation) {
-    return res.status(404).send({ error: "Invalid updates!" });
+  if (!isValidOperation)
+  {
+    return res.status(404).send({ error: 'Invalid updates!' });
   }
 
-  try {
-    updates.forEach((update) => (req.user[update] = req.body[update]));
+  try
+  {
+    updates.forEach((update) => req.user[update] = req.body[update]);
     await req.user.save();
     res.send(req.user);
-  } catch (error) {
+  }
+  catch (error)
+  {
     // console.error("Update error:", error);
     res.status(400).send({ error: "Update failed" });
   }
 });
 
 // Delete Route
-router.delete("/doctor/me", checkExpiration, auth, async (req, res) => {
-  try {
+router.delete("/doctor/me", checkExpiration, auth, async (req, res) =>
+{
+  try
+  {
     const doctorId = req.user._id;
+    const deletedDoctor = await Doctor.findByIdAndDelete(doctorId);
 
-    res.send({ message: "Account deleted successfully." });
-  } catch (error) {
+    res.send({ message: 'Account deleted successfully.' });
+  } catch (error)
+  {
+    // console.error("Delete error:", e);
     res.status(500).send({ error: "Delete failed" });
   }
 });
 
 // Logout Route
-router.post("/doctor/logout", auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
+router.post("/doctor/logout", checkExpiration, auth, async (req, res) =>
+{
+  try
+  {
+    req.user.tokens = req.user.tokens.filter((token) => { return token.token !== req.token; });
     await req.user.save();
     res.send({ message: "Logout successful" });
-  } catch (e) {
+  }
+
+  catch (e)
+  {
     // console.error("Logout error:", e);
     res.status(500).send({ error: "Logout failed" });
   }
 });
 
 // Logout All Route - Logout a doctor from all devices
-router.post("/doctor/logoutall", checkExpiration, auth, async (req, res) => {
-  try {
+router.post("/doctor/logoutall", auth, async (req, res) =>
+{
+  try
+  {
     req.user.tokens = [];
     await req.user.save();
     res.send({ message: "Logout successful from all instances." });
-  } catch (error) {
+  }
+  catch (error)
+  {
     // console.error("Logout error:", error);
     res.status(500).send({ error: "Logout failed" });
   }
 });
 
 // Read Doctor Route
-router.get("/doctor/me", checkExpiration, auth, async (req, res) => {
+router.get("/doctor/me", checkExpiration, auth, async (req, res) =>
+{
   res.send(req.user);
 });
 
 // assignDoctor Route
-router.post("/doctor/assignDoctor", checkExpiration, auth, async (req, res) => {
-  try {
+router.post("/doctor/assignDoctor", checkExpiration, auth, async (req, res) =>
+{
+  try
+  {
     const doctorId = req.user._id;
     const patientEmail = req.body.email;
 
     const patient = await Patient.findOne({ email: patientEmail });
 
-    if (!patient) {
+    if (!patient)
+    {
       return res.status(404).send({ error: "Patient not found" });
     }
 
     const result = await assignDoctor(patient._id, doctorId);
-    if (result.error) {
+    if (result.error)
+    {
       return res.status(400).send({ error: result.message });
     }
 
     res.status(201).send({ message: "Doctor assigned successfully" });
-  } catch (error) {
+  }
+  catch (error)
+  {
     // console.error("Assign doctor error:", error);
     res.status(500).send({ error: "Doctor assignment failed" });
   }
 });
 
 // removeDoctor Route
-router.delete("/doctor/removeDoctor", checkExpiration, auth, async (req, res) => {
-  try {
+router.delete("/doctor/removeDoctor", checkExpiration, auth, async (req, res) =>
+{
+  try
+  {
     const doctorId = req.user._id;
     const patientEmail = req.body.email;
 
     const patient = await Patient.findOne({ email: patientEmail });
-    if (!patient) {
+    if (!patient)
+    {
       return res.status(404).send({ error: "Patient not found" });
     }
 
     const result = await removeDoctor(patient._id, doctorId);
-    if (result.error) {
+    if (result.error)
+    {
       return res.status(400).send({ error: result.message });
     }
 
     res.status(200).send({ message: "Doctor removed successfully" });
-  } catch (error) {
+  }
+  catch (error)
+  {
     // console.error("Remove doctor error:", error);
     res.status(400).send({ error: "Doctor removal failed" });
   }
