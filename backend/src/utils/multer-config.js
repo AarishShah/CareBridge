abdd4ba0e132cd18a765df71daeed182f9e6752d
humma-irshad <hummairshad48@gmail.com>
2024-05-29 23:29:33 +0530

    Add multer configurations for profile picture uploads

---
+++ backend/src/utils/multer-config.js
@@ -0,0 +1,45 @@
+const { join } = require("path")
+const fs = require("fs");
+const multer = require("multer");
+
+// To make sure the directory exists
+const profilePictureDir = join(__dirname, "../../uploads/profile-pictures");
+if (!fs.existsSync(profilePictureDir))
+{
+	fs.mkdirSync(profilePictureDir, { recursive: true });
+}
+// Storage location
+const profileStorage = multer.diskStorage({
+	destination: (_, file, callback) =>
+	{
+		callback(null, profilePictureDir);
+  	},
+  	filename: (_, file, callback) =>
+	{
+    	callback(null, file.originalname);
+  },
+});
+
+const imageFilter = (_, file, callback) =>
+{
+	if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
+	{
+		return callback(new Error("Only image files are allowed"), false);
+	}
+
+	callback(null, true);
+} 
+
+// Adding file handling middleware
+// single: handles one file at a time
+const profileUpload = multer({
+	storage: profileStorage,
+  	fileFilter: imageFilter,
+  	limits: { fileSize: 1000000 }, // 1MB limit
+}).single("profile");
+
+// For medical documents
+// Incomplete
+const documentUpload = multer({ dest: "uploads/documents" }).single("document");
+
+module.exports = { profileUpload, documentUpload };
