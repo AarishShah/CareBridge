4469c2b24fa332352b64be0c46841a0603d90cbb
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2024-05-30 19:08:34 +0530

    changes

--- frontend/src/components/DynamicSection.jsx
+++ frontend/src/components/DynamicSection.jsx
@@ -27,7 +27,7 @@ export default function DynamicSection() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 8000, // Slow speed for continuous sliding
+    speed: 10000, // Slow speed for continuous sliding
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
