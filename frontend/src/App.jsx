47400ef3ba3bd047fcfa2d5235a3c75bc5e69478
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2024-06-22 21:06:00 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './components/context/AuthContext';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './components/context/AuthContext';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -41,11 +38,7 @@ function App() {
         <RouterProvider router={router} />
       </AuthProvider>
     </QueryClientProvider>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
+    
   );
 }
 
--- frontend/src/components/Moto.jsx
+++ frontend/src/components/Moto.jsx
@@ -26,7 +26,7 @@ const Moto = () => {
     <div className="flex flex-col items-center mt-6 lg:mt-20">
         <h1 className="text-3xl sm:text-6xl lg:text-6xl text-center tracking-wide">
         Empower Healthcare Decisions
-            <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">{" "}with CareBridge</span> 
+            <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">{" "} <br /> with CareBridge</span> 
         </h1>
         <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">Transform your clinical practice with intelligent insights.
 Unlock the potential of your medical expertise with our advanced decision support tools. Experience the future of healthcare today with CareBridge.</p>
