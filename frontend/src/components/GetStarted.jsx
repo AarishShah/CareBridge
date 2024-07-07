5e476beffa972d3e6110bb9f1fb79329db4a590a
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2024-07-07 18:49:06 +0530

    refactor: Improve readability and responsiveness of GetStarted and PersonalInfoPatient components

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -7,7 +7,9 @@ export default function GetStarted() {
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 md:px-8 py-8 rounded-lg">
         <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-black mb-4 md:mb-6">Elevate Your Medical Practice</h1>
         <p className="text-sm md:text-md lg:text-lg text-black mb-2 md:mb-4 mt-4 md:mt-6">
-          Join CareBridge and transform your practice with advanced clinical tools. Stay ahead with the latest medical guidelines and expert insights to enhance patient outcomes.
+          Join CareBridge and transform <br />
+          your practice with advanced clinical tools. <br /> Stay ahead with the latest medical guidelines and expert <br />
+           insights to enhance patient outcomes.
         </p>
         <p className="text-sm md:text-md lg:text-lg text-black">
           Looking to elevate your practice?
