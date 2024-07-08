dd9e0763222f6c1b472de776ce61b80f4422ce87
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2024-07-08 15:34:45 +0530

    changes

--- frontend/src/components/auth/AuthForm.jsx
+++ frontend/src/components/auth/AuthForm.jsx
@@ -4,7 +4,7 @@ import image1 from '../../assets/2.png';
 // eslint-disable-next-line react/prop-types
 const AuthForm = ({ heading, children, onSubmit }) => {
   return (
-    <div className='flex flex-col md:flex-row h-screen'>
+    <div className='flex flex-col md:flex-row min-h-screen h-full'>
       <img src={image} alt="" className='hidden md:block md:w-1/2 object-cover' />
       <div className='flex flex-col items-center w-full md:w-1/2 p-4 md:p-8'>
         <img src={image1} alt="" className='h-16 w-16 rounded-full mt-16 mb-8' />
