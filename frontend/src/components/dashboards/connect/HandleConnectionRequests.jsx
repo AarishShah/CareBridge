0e479d169990314c18fa61699e005778002e5a32
h <76107314+humma-irshad@users.noreply.github.com>
2024-08-02 15:38:46 +0530

    Update HandleConnectionRequests.jsx

--- frontend/src/components/dashboards/connect/HandleConnectionRequests.jsx
+++ frontend/src/components/dashboards/connect/HandleConnectionRequests.jsx
@@ -33,7 +33,7 @@ const HandleConnectionRequest = ({ requestId }) => {
 
   return (
     <div className="mt-4">
-      {location.pathname.includes("patient/view-incoming-requests") && (
+      {location.pathname.includes("patient/view-incoming-requests") || location.pathname.includes("doctor/view-incoming-requests") && (
         <button
           className="mr-2 px-4 py-2 bg-green-500 text-white font-semibold rounded-md shadow hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
           onClick={() => handleResponse("accept")}
